2bb9ea6746b6169402f87388667e90e501fe8342
maidat <maidat760@gmail.com>
2026-03-06 13:05:08 +0700

    First commit

--- PROJECT_TEAM1_PRN222/Controllers/HomeController.cs
+++ PROJECT_TEAM1_PRN222/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using PROJECT_TEAM1_PRN222.Models;
 
 namespace PROJECT_TEAM1_PRN222.Controllers
 {
+    // test git
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
