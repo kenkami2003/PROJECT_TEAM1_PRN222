93f460025d5923a9a98795c01aa306394c31c1c1
dangwkhiem <dangwkhiem.work@gmail.com>
2026-03-21 18:14:29 +0700

    Thêm PortalController và Layout cho Admin

--- PROJECT_TEAM1_PRN222/Program.cs
+++ PROJECT_TEAM1_PRN222/Program.cs
@@ -1,20 +1,25 @@
-var builder = WebApplication.CreateBuilder(args);
+using BoardingHouseManagement.Models;
+using Microsoft.EntityFrameworkCore;
 
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
 }
-app.UseStaticFiles();
 
+app.UseStaticFiles();
 app.UseRouting();
 
-app.UseAuthorization();
+app.MapControllerRoute(
+    name: "admim",
+    pattern: "Admin/{controller=Portal}/{action=Index}/{id?}");
 
 app.MapControllerRoute(
     name: "default",
