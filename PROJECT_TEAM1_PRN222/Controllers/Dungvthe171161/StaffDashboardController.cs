9b786ba4103a4eb9c73b00816a4e8799c6befa0a
dungvthe171161-wq <dungvthe171161@fpt.edu.vn>
2026-03-25 14:11:27 +0700

    m
    m

---
+++ PROJECT_TEAM1_PRN222/Controllers/Dungvthe171161/StaffDashboardController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BoardingHouseManagement.Models;
+
+namespace PROJECT_TEAM1_PRN222.Controllers.Dungvthe171161
+{
+    public class StaffDashboardController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public StaffDashboardController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.TotalRequests = _context.MaintenanceRequests.Count();
+            ViewBag.OpenRequests = _context.MaintenanceRequests.Count(x => x.Status == RequestStatus.Open);
+            ViewBag.Processing = _context.MaintenanceRequests.Count(x => x.Status == RequestStatus.Processing);
+            ViewBag.Fixed = _context.MaintenanceRequests.Count(x => x.Status == RequestStatus.Fixed);
+
+            ViewBag.TotalLogs = _context.AuditLogs.Count();
+
+            return View("~/Views/Dungvthe171161/StaffDashboard/Index.cshtml");
+        }
+    }
+}
